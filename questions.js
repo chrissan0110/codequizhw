acd971ee2da746f4fa452f5a5204ef5f4b36fcee
Christopher Sanford <chrissan0110@gmail.com>
2019-12-17 22:42:24 -0600

    added in questions/array

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Ketchup's main ingredient is:",
+        choices: ["Tomatoes", "Beans", "Cherries", "Rocks"],
+        answer: "Tomatoes"
+    },
+    {
+        title: "A burger is most commonly between two:",
+        choices: ["patties", "onions", "buns", "grills"],
+        answer: "buns"
+    },
+    {
+        title: "What is the most popular Texas cuisine?",
+        choices: ["BBQ", "Pasta", "Chili", "Hot Dogs"],
+        answer: "BBQ"
+    },    
+    {
+        title: "This food is most commonly associated with salmonella:",
+        choices: ["eggs", "meat", "poultry", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "Ketchup was sold as what in the 1830's?",
+        choices: ["Skin moisturizer", "medicine", "syrup", "soap"],
+        answer: "medicine"
+    }
+  ];
