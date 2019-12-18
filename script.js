c274f9e2630281e0f5704dceae664c7cded81887
Christopher Sanford <chrissan0110@gmail.com>
2019-12-17 23:34:12 -0600

    added functions if/else statements and alterts for correct or incorect

--- script.js
+++ script.js
@@ -42,3 +42,44 @@ startBtn.addEventListener('click', function() {
     document.querySelector('.quizPage').style.display='none';
     displayQuestions(questionIndex);
 })
+
+var questionIndex = 0;
+
+function displayQuestions(){
+    
+    questionsEl.textContent = "";
+    var question = questions[questionIndex];
+    var questionDiv = document.createElement("div");
+    var questionText = document.createElement("h2");
+    questionText.textContent = question.title;
+    questionDiv.appendChild(questionText)
+    
+for (var i=0; i < question.choices.length ;i++) {
+    var option = document.createElement("button");
+    option.textContent = question.choices[i];
+    option.setAttribute("class", "option");
+    option.addEventListener("click", function(e) {
+    var optionClicked = (e.target.innerHTML);
+            
+        if(optionClicked === questions[questionIndex].answer){
+            alert("Correct!");
+            displayQuestions(questionIndex++);
+        }
+        else{
+            alert("Incorrect!");
+            displayQuestions(questionIndex++);
+            secondsLeft -= 10;
+        }
+            
+        });
+        if (questionIndex == questions.length - 1) {
+            clearInterval(timerInterval);
+            document.querySelector('.myScore').style.display='block';
+            document.querySelector(".Time").style.display='none';
+            highScoreEl.textContent = "YOUR SCORE IS " + secondsLeft;
+            return;
+        }
+        questionDiv.appendChild(option);
+    }
+    questionsEl.appendChild(questionDiv);
+}
